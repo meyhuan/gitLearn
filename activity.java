59d2705917fd0e3ff9fec5e3fa62e782dd32d435
yuanguanhuan <yuanguanhuan@cvte.com>
2017-07-04 20:28:28 +0800

    add something at local

--- activity.java
+++ activity.java
@@ -3,4 +3,5 @@ public void main(){
 	println("add something on side2");
 	println("add something on side2 second");
 	println("add something on side1 on master")
+	println("add sommthin at side1 on local");
 }
