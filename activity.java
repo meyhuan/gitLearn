4a55036b2a56d420c6f657d81cadcdee7a76aa9d
meyhuan <627655140@qq.com>
2017-07-04 20:40:24 +0800

    Update activity.java

--- activity.java
+++ activity.java
@@ -3,8 +3,5 @@ public void main(){
 	println("add something on side2");
 	println("add something on side2 second");
 	println("add something on side1 on master")
-<<<<<<< HEAD
 	println("add sommthin at side1 on local");
-=======
->>>>>>> 9c37855dd4bcf8777e1e313b6c7453681d71c6a2
 }
